a7ed674c8c441e828e0a06e340a67473371ec05f
ivanguzman6 <ivanguzman6@hotmail.com>
2015-08-11 13:54:02 -0400

    manejo de los hijos
    Se agrego el state.go a la pagina 1
    Se agrego navegador con tabs a pagina 2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('pagina1Ctrl', function($scope) 
+.controller('pagina1Ctrl', function($scope,$state) 
 {
 	$scope.nombre="ivan";
+	
+	$scope.salvar  = function() 
+	{
+		alert("Datos Salvados");
+		$state.go('pagina2.hijo2');
+	};
 })
 
 .controller('hijo1Ctrl', function($scope) 
